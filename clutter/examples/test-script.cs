a6def14752e19fb75fb7bcd2f8253a7b9a159942
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2008-01-08 17:38:15 +0000

    Add script test

---
+++ clutter/examples/test-script.cs
@@ -0,0 +1,87 @@
+using System;
+using Clutter;
+using Gdk;
+
+public class ShaderTest
+{
+	static string test_behaviour = "[" + 
+		"{"									 + 
+		"    \"id\" : \"main-timeline\","                                        + 
+		"    \"type\" : \"ClutterTimeline\","                                    + 
+		"    \"duration\" : 5000,"                                               + 
+		"    \"loop\" : true"                                                    + 
+		"  },"                                                                   + 
+		"  {"                                                                    + 
+		"    \"id\"          : \"path-behaviour\","                              + 
+		"    \"type\"        : \"ClutterBehaviourPath\","                        + 
+		"    \"knots\"       : [ [ 50, 50 ], { \"x\" : 100, \"y\" : 100 } ],"    + 
+		"    \"alpha\"       : {"                                                + 
+		"      \"timeline\" : \"main-timeline\","                                + 
+		"      \"function\" : \"ramp\""                                          + 
+		"    }"                                                                  + 
+		"  },"                                                                   + 
+		"  {"                                                                    + 
+		"    \"id\"          : \"rotate-behaviour\","                            + 
+		"    \"type\"        : \"ClutterBehaviourRotate\","                      + 
+		"    \"angle-start\" : 0.0,"                                             + 
+		"    \"angle-end\"   : 360.0,"                                           + 
+		"    \"axis\"        : \"y-axis\","                                      + 
+		"    \"alpha\"       : {"                                                + 
+		"      \"timeline\" : \"main-timeline\","                                + 
+		"      \"function\" : \"sine\""                                          + 
+		"    }"                                                                  + 
+		"  },"                                                                   + 
+		"  {"                                                                    + 
+		"    \"id\"            : \"fade-behaviour\","                            + 
+		"    \"type\"          : \"ClutterBehaviourOpacity\","                   + 
+		"    \"opacity-start\" : 255,"                                           + 
+		"    \"opacity-end\"   : 0,"                                             + 
+		"    \"alpha\"         : {"                                              + 
+		"      \"timeline\" : \"main-timeline\","                                + 
+		"      \"function\" : \"ramp-inc\""                                      + 
+		"    }"                                                                  + 
+		"  }"                                                                    + 
+		"]";
+
+	static string test_unmerge = 
+	        "{"                                  +  
+		"   \"id\" : \"blue-button\",       "+ 
+		"   \"type\" : \"ClutterRectangle\","+ 
+		"   \"color\" : \"#0000ffff\",      "+ 
+		"   \"x\" : 350,                    "+ 
+		"   \"y\" : 50,                     "+ 
+		"   \"width\" : 100,                "+ 
+		"   \"height\" : 100,               "+ 
+		"   \"visible\" : true,             "+ 
+		"   \"reactive\" : true             "+ 
+		" }";
+
+	static Script script;
+	static uint merge_id = 0;
+
+	private static void HandleButtonPress(object sender, ButtonPressEventArgs args)
+	{
+	 	Console.WriteLine("Unmerging");
+		script.UnmergeObjects(merge_id);
+	}
+
+	public static void Main ()
+	{
+		ClutterRun.Init (); 
+
+		script = new Script ();
+		script.LoadFromData (test_behaviour);
+		script.LoadFromFile ("test-script.json");
+		merge_id = script.LoadFromData (test_unmerge);
+
+		Stage stage = (Stage)script.GetObject("main-stage");
+		Actor red_button = (Actor)script.GetObject("red-button");
+
+		red_button.ButtonPressEvent += HandleButtonPress;
+
+		stage.ShowAll ();
+
+		ClutterRun.Main ();
+	} 
+}
+
